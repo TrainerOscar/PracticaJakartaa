5544f8a90854a148baf527079e79d5d173398c8b
TrainerOscar <ogcalero@uamv.edu.ni>
2024-10-07 19:22:30 -0600

    Agrego clase User

---
+++ src/main/java/models/User.java
@@ -0,0 +1,27 @@
+
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Tb_User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_user;
+
+    @Column(name = "username", length = 15, nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", length = 25, nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role", referencedColumnName = "id_role", nullable = false)
+    private Role role;
+
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "status", nullable = false)
+    private boolean status;
+}
